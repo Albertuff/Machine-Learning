80aead286e7d76fb6c51938ad98745cd5f3d9b37
Alberto <albertuff@gmail.com>
2022-11-04 18:47:22 -0600

    Ejercicio 1 diabetes

---
+++ Modulo 1/Ejercicio 1 - Clase 5.py
@@ -0,0 +1,7 @@
+# Sistema de predicción  para el diagnostico de predicción de diabetes.
+# Utilizando el algoritmo de k vecinos mas cercanos
+
+# Paso 0: Cargar las librerias
+
+import pandas as pd
+import matplotlib as plt
