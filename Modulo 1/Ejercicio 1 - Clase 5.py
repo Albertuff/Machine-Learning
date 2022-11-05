68e22fa1da886130a0a1072f9e4496c6f7070e56
Alberto <albertuff@gmail.com>
2022-11-04 18:55:52 -0600

    datos

--- Modulo 1/Ejercicio 1 - Clase 5.py
+++ Modulo 1/Ejercicio 1 - Clase 5.py
@@ -4,4 +4,13 @@
 # Paso 0: Cargar las librerias
 
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import metrics 
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import plot_confusion_matrix
+
+# Paso 1: Cargar los datos
+
+datos=pd.read_csv("https://raw.githubusercontent.com/Albertuff/Machine-Learning/master/datos/diabetes.csv")
+
