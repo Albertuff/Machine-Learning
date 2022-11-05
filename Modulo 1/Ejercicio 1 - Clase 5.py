91b44ef61d5058f6687ebf969d6f301780985f3b
Alberto <albertuff@gmail.com>
2022-11-04 19:42:24 -0600

    modelo terminado

--- Modulo 1/Ejercicio 1 - Clase 5.py
+++ Modulo 1/Ejercicio 1 - Clase 5.py
@@ -9,8 +9,33 @@ import numpy as np
 from sklearn import metrics 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import plot_confusion_matrix
+from sklearn.model_selection import train_test_split # Para partir el conjunto de datos en entrenamiento y prueba de manera automatica
 
 # Paso 1: Cargar los datos
 
 datos=pd.read_csv("https://raw.githubusercontent.com/Albertuff/Machine-Learning/master/datos/diabetes.csv")
 
+# Definimos los atributos(features) y las variable objeivo(target)
+
+X=datos[["glucose","insulin","sspg"]] # Atributos
+Y=datos["class"]                      # Respuesta
+
+# Paso 1.1: Definimos los conjuntos de prueba y entrenamiento
+
+# Nota: train_test_split(atributos,target,train_size=porcion de los datos que se usan para entrenar,random_state=semilla para reproducir los resultados)
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,train_size=0.80,random_state=(123))
+
+# Paso 2: Entrenar el modelo K_vecinos
+
+K_vecinos=KNeighborsClassifier(n_neighbors=3,n_jobs=-1).fit(X_train,Y_train)
+
+# Paso 3: Hacer las predicciones 
+
+predicciones=K_vecinos.predict(X_test)
+
+# Paso 4; Evaluar el desempeño predictivo del modelo
+# Necesitamos definir una metrica, por ahora sera la exactitud o accuracy
+
+print(metrics.accuracy_score(Y_test,predicciones))
+plot_confusion_matrix(K_vecinos,X_test,Y_test)
+plt.show()
